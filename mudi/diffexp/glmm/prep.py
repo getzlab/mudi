2b5afcf2e3694f1eee4e801c418f51bb5d9362f8
Shankara <sanand@broadinstitute.org>
2020-02-13 01:11:19 +0000

    updated prep for lme4

--- mudi/diffexp/glmm/prep.py
+++ mudi/diffexp/glmm/prep.py
@@ -6,15 +6,27 @@ import glob
 import subprocess
 import scanpy as sc
 from agutil import parallel
+from anndata import AnnData
+from typing import Union
 
-def prep_inputs(adata, diffexp_dir, meta_vars=None, genes_to_use=None, n_threads=15):
+def prep_inputs(
+    adata: AnnData,
+    diffexp_dir: str,
+    groupby: str,
+    meta_vars: Union[None,list] = None,
+    genes_to_use: Union[None, list] = None,
+    n_threads: int = 15
+    ):
     """
     Prepare Inputs for Differential Expression
     -------------------------------
     Args:
-        * adata
-        * diffexp_dir
+        * adata: AnnData file
+        * diffexp_dir: output directory to write out inptus for lme4 to
+        * groupby: variable to groupy
         * meta_vars: list of variables in metadata
+        * genes_to_use: list of genes to use for differential expression tests
+        * n_threads: nthreads for saving gene counts
 
     Returns:
         None
@@ -26,6 +38,8 @@ def prep_inputs(adata, diffexp_dir, meta_vars=None, genes_to_use=None, n_threads
 
     for v in meta_vars:
         assert v in list(adata.obs), print("{} not in adata.obs.".format(v))
+
+    assert groupby in metavars, "{} not in metavars".format(groupby)
 
     os.makedirs(diffexp_dir, exist_ok=True)
     os.makedirs(os.path.join(diffexp_dir, "inputs"), exist_ok=True)
@@ -47,9 +61,13 @@ def prep_inputs(adata, diffexp_dir, meta_vars=None, genes_to_use=None, n_threads
     counts_df.T.to_csv(os.path.join(diffexp_dir, "inputs", "raw_counts.csv"))
     counts_df.T.to_parquet(os.path.join(diffexp_dir, "inputs", "raw_counts.parquet"))
 
-    adata.obs[meta_vars].rename(
+    meta_df = adata.obs[meta_vars].rename(
         columns={'log1p_n_genes_by_counts':'loggenesxcounts'}
-        ).to_csv(os.path.join(diffexp_dir, "inputs", "meta.csv"))
+    )
+
+    dummy_df = pd.get_dummies(meta_df[groupby])
+    dummy_df = dummy_df.rename(columns={x:"groupby_"+x for x in dummy_df})
+    meta_df.join(dummy_df).to_csv(os.path.join(diffexp_dir, "inputs", "meta.csv"))
 
     @parallel.parallelize(maximum=n_threads)
     def save_slice(gene_i):
