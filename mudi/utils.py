ff566f059fc6ee034b6ad91871966d49210cf8bf
shankara-a <sanand@broadinstitute.org>
2021-10-15 16:50:38 +0000

    updates to marker aggr functions

--- mudi/utils.py
+++ mudi/utils.py
@@ -30,30 +30,83 @@ def scanpy_adata_loader(path, genome='GRCh38', verbose=True):
         return tmp[0].concatenate(tmp[1:])
 
     if os.path.isfile(path):
-        return sc.read_10x_h5(path, genome=genome)
+        ad = sc.read_10x_h5(path, genome=genome)
+        ad.var_names_make_unique()
+        return ad
     elif os.path.isdir(path):
-        return sc.read_10x_mtx(path)
+        ad = sc.read_10x_mtx(path)
+        ad.var_names_make_unique()
+        return ad
     else:
         raise FileError("Provide proper path.")
 
-def aggr_markers(adata, uns='rank_genes_groups', params=['names','scores','pvals','pvals_adj','logfoldchanges']):
+def get_percent_expr(adata, groupby):
+    """
+    Get percent expressed & mean expression.
+    ------------------------------
+    Requires:
+        * adata.layers['counts'] -> counting percent of cells with gene expressed
+        * adata.raw.X -> for computing mean expression (log1p)
+    """
+    from tqdm import tqdm
+
+    groups = list(adata.obs[groupby].cat.categories)
+    res_in = pd.DataFrame(columns=adata.var_names, index=groups)
+    res_out = pd.DataFrame(columns=adata.var_names, index=groups)
+    res_mean_in = pd.DataFrame(columns=adata.var_names, index=groups)
+    res_mean_out = pd.DataFrame(columns=adata.var_names, index=groups)
+
+    for group in tqdm(groups, desc="Computing metrics per group"):
+        res_in.loc[group] = (adata[adata.obs[groupby].isin([group]),:].layers['counts'] > 0).mean(0)
+        res_out.loc[group] = (adata[~adata.obs[groupby].isin([group]),:].layers['counts'] > 0).mean(0)
+        res_mean_in.loc[group] = adata[adata.obs[groupby].isin([group]),:].raw.X.mean(0)
+        res_mean_out.loc[group] = adata[~adata.obs[groupby].isin([group]),:].raw.X.mean(0)
+
+    res_in = res_in.T
+    res_out = res_out.T
+    res_mean_in = res_mean_in.T
+    res_mean_out = res_mean_out.T
+
+    res_in = res_in.reset_index().melt(id_vars=['index']).rename(columns={'index':'names','variable':"group", 'value':'percent_in'})
+    res_out = res_out.reset_index().melt(id_vars=['index']).rename(columns={'index':'names','variable':"group", 'value':'percent_out'})
+
+    res_mean_in = res_mean_in.reset_index().melt(id_vars=['index']).rename(columns={'index':'names','variable':"group", 'value':'mean_expr_in'})
+    res_mean_out = res_mean_out.reset_index().melt(id_vars=['index']).rename(columns={'index':'names','variable':"group", 'value':'mean_expr_out'})
+
+    return pd.merge(pd.merge(res_in, res_out), pd.merge(res_mean_in, res_mean_out))
+
+def aggr_markers(adata, uns='rank_genes_groups', expr_metrics=True):
     """
     Aggregate markers.
     ------------------
     Returns an easy to view marker list dataframe.
     Assumes 'rank_genes_groups' has already been called to find group markers
     in AnnData Object.
+        * expr_metrics -> compute percent of cells expressed & mean expression for in/out groups.
     """
     assert adata.uns[uns], 'Compute differentially expressed genes first.'
 
-    joint = pd.concat(
-        [pd.DataFrame(pd.DataFrame(adata.uns[uns][param]).T.stack()).reset_index().rename(columns={'level_0':'cluster','level_1':'no.',0:param})
-            for param in params],
-        1)
+    aggr_df = sc.get.rank_genes_groups_df(adata, None)
 
-    return joint.loc[:,~joint.columns.duplicated()]
+    if expr_metrics:
+        aggr_percent_expr = get_percent_expr(adata, adata.uns[uns]['params']['groupby'])
+        return pd.merge(aggr_df, aggr_percent_expr)
+    else:
+        return aggr_df
 
-def get_uns(adata,tag):
+def get_de_genes_to_plot(markers_df, lfc_thresh=1, padj_thresh=0.1, n_to_plot=5):
+    """
+    Top DiffExp Genes.
+    Return as dict for easy plotting with sc.pl.dotplot.
+    """
+    markers_df = markers_df[
+        (markers_df['logfoldchanges']>=lfc_thresh) &
+        (markers_df['pvals_adj']<=padj_thresh)
+    ].groupby("group").head(n_to_plot)
+
+    return markers_df.groupby("group").agg(list)['names'].to_dict()
+
+def get_uns(adata, tag):
     """
     Retrieve unstructured data stored in AnnData.
     ------------------------
@@ -70,6 +123,21 @@ def get_uns(adata,tag):
     except:
         raise ValueError("Unable to return structured dataframe from data.uns[{}]".format(tag))
 
+def get_a_by_b(adata, a, b, norm=False):
+    """
+    Get A x B.
+    ----------------
+    Number of each .obs b per .obs a
+        returns pd.Dataframe
+    """
+    hm = adata.obs.groupby([a,b]).size().reset_index().set_index(a).pivot(columns=b)
+
+    if norm:
+        hm = hm.div(hm.sum(1), 0)
+
+    hm.columns = hm.columns.droplevel()
+    hm.columns.name = None
+    return hm
 # ---------------------------------
 # Utilities
 # ---------------------------------
